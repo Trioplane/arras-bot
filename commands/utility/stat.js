1f4586dc46fdebe0b8e4bbeb622a2ff71a645060
Trplnr <87922281+Trioplane@users.noreply.github.com>
2023-09-19 18:05:46 +0800

    uses canvas insteadd of @napi-rs/canvas

--- commands/utility/stat.js
+++ commands/utility/stat.js
@@ -1,22 +1,34 @@
 import { SlashCommandBuilder, AttachmentBuilder } from "discord.js";
-import Canvas from '@napi-rs/canvas'
+import { createCanvas } from "canvas";
+
+const applyText = (canvas, text) => {
+	const context = canvas.getContext('2d');
+
+	// Declare a base size of the font
+	let fontSize = 70;
+
+	do {
+		// Assign the font to the context and decrement it so it can be measured again
+		context.font = `${fontSize -= 10}px sans-serif`;
+		// Compare pixel width of the text to the canvas minus the approximate avatar size
+	} while (context.measureText(text).width > canvas.width - 300);
+
+	// Return the result to use in the actual canvas
+	return context.font;
+};
 
 const commandData = new SlashCommandBuilder()
                     .setName('stat')
                     .setDescription('lorem ipsum dolor sit amet.')
 
 const executeFunction = async (interaction) => {
-    const canvas = Canvas.createCanvas(1000, 1000);
+    const canvas = createCanvas(1000, 1000);
 	const context = canvas.getContext('2d');
 
-    context.fillStyle = 'black'
+    context.fillStyle = 'lavender'
     context.fillRect(0, 0, canvas.width, canvas.height)
 
-    context.font = '100px sans-serif';
-	context.fillStyle = 'white';
-	context.fillText('test', canvas.width / 2, canvas.height / 2);
-
-    const attachment = new AttachmentBuilder(await canvas.encode('png'), { name: 'stats.png' });
+    const attachment = new AttachmentBuilder(canvas.toBuffer('image/png', { compressionLevel: 1, filters: canvas.PNG_FILTER_NONE }), { name: 'stats.png' });
     await interaction.reply({ files: [attachment] })
 }
 
